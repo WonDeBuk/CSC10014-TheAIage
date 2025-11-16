a5e0c9042a81c97756e7fbaf070e0936f0db0f04
hienlong <hienlong106@gmail.com>
2025-11-16 10:48:07 +0000

    Scrollable form, while maintaining the background in the RegisterPage.

--- Client/src/presentation/pages/RegisterPage/RegisterPage.tsx
+++ Client/src/presentation/pages/RegisterPage/RegisterPage.tsx
@@ -12,7 +12,6 @@ export default function RegisterPage() {
     const [formData, setFormData] = useState<FormData>({ name: "", email: "", password: "", role: "" });
     const [errorMessage, setErrorMessage] = useState<string>("");
 
-
     const navigate = useNavigate();
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -36,13 +35,14 @@ export default function RegisterPage() {
     };
 
     return (
-        <div className="bg-[linear-gradient(to_right,rgba(196, 220, 89, 0.7),rgba(147,51,234,0.7)),url('assets/login_bg.png')] bg-cover bg-center flex flex-col items-center gap-10 w-dvw h-dvh bg-[#edeffd] fixed top-0">
-            <div className="mt-8">
+        //add scroll to overflow-y-auto
+        <div className="bg-[linear-gradient(to_right,rgba(196, 220, 89, 0.7),rgba(147,51,234,0.7)),url('assets/login_bg.png')] bg-cover bg-center flex flex-col items-center w-dvw h-dvh bg-[#edeffd] fixed top-0 overflow-y-auto">
+            <div className="mt-8 mb-8">
                 <p className="text-[36px] text-center">User Registration</p>
                 <p className="text-[18px] text-center text-[#a2abb7]">Welcome to our website</p>
             </div>
 
-            <div className="flex flex-col items-start bg-[#fffeff] gap-8 p-5 rounded-lg min-w-2/7">
+            <div className="flex flex-col items-start bg-[#fffeff] gap-8 p-5 rounded-lg min-w-2/7 mb-8 max-h-[80vh] overflow-y-auto">
                 <div className="w-full">
                     <div className="flex items-end justify-between">
                         <p className="text-[22px] font-[500]">Register</p>
@@ -67,26 +67,26 @@ export default function RegisterPage() {
                             <label className="block text-[18px] font-[400]" htmlFor="password">Password:</label>
                             <input onChange={handleChange} value={formData.password} type="password" name="password" className={`w-full text-sm h-[20px] bg-[#efebef] min-h-10 rounded-lg p-3 ${formData.name ? "text-black" : "text-[#8c8097]"}`} placeholder="Your password should be at least 8 character-long." />
                         </div>
-                        <div className="w-full">
-                            <p className="block text-[18px] font-[400]">Role:</p>
-                            <div className="w-full flex justify-between items-center">
-                                <div className={`flex justify-start items-center gap-1  px-5 rounded-lg ${formData.role === "" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "" }))}>
-                                    <input onChange={handleChange} value="" checked={formData.role === ""} type="radio" name="role" className="w-full text-sm h-[20px] bg-[#6f6c81] text-white min-h-10 rounded-lg p-3" placeholder="Password should be at least 8 character-long." />
-                                    <label className="text-[15px] font-[400]" htmlFor="role">None</label>
+                        <div className="w-full mb-6">
+                            <p className="block text-[18px] font-[400] mb-3">Role:</p>
+                            <div className="w-full flex flex-col gap-2">
+                                <div className={`flex justify-start items-center gap-2 px-5 py-2 rounded-lg cursor-pointer ${formData.role === "" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "" }))}>
+                                    <input onChange={handleChange} value="" checked={formData.role === ""} type="radio" name="role" className="cursor-pointer" />
+                                    <label className="text-[15px] font-[400] cursor-pointer" htmlFor="role">None</label>
                                 </div>
-                                <div className={`flex justify-start items-center gap-1  px-5 rounded-lg ${formData.role === "student" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "student" }))}>
-                                    <input onChange={handleChange} value="student" checked={formData.role === "student"} type="radio" name="role" className="w-full text-sm h-[20px] bg-[#6f6c81] text-white min-h-10 rounded-lg p-3" placeholder="Password should be at least 8 character-long." />
-                                    <label className="text-[15px] font-[400]" htmlFor="role">Student</label>
+                                <div className={`flex justify-start items-center gap-2 px-5 py-2 rounded-lg cursor-pointer ${formData.role === "student" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "student" }))}>
+                                    <input onChange={handleChange} value="student" checked={formData.role === "student"} type="radio" name="role" className="cursor-pointer" />
+                                    <label className="text-[15px] font-[400] cursor-pointer" htmlFor="role">Student</label>
                                 </div>
-                                <div className={`flex justify-start items-center gap-1  px-5 rounded-lg ${formData.role === "counsellor" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "counsellor" }))}>
-                                    <input onChange={handleChange} value="counsellor" checked={formData.role === "counsellor"} type="radio" name="role" className="w-full text-sm h-[20px] bg-[#6f6c81] text-white min-h-10 rounded-lg p-3" placeholder="Password should be at least 8 character-long." />
-                                    <label className="text-[15px] font-[400]" htmlFor="role">Counsellor</label>
+                                <div className={`flex justify-start items-center gap-2 px-5 py-2 rounded-lg cursor-pointer ${formData.role === "counsellor" ? "bg-black text-white" : "bg-[#efebef] text-black"}`} onClick={() => setFormData((prev) => ({ ...prev, role: "counsellor" }))}>
+                                    <input onChange={handleChange} value="counsellor" checked={formData.role === "counsellor"} type="radio" name="role" className="cursor-pointer" />
+                                    <label className="text-[15px] font-[400] cursor-pointer" htmlFor="role">Counsellor</label>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full mt-8">
-                        <input type="submit" className="text-white text-sm bg-black h-[50px] text-center w-full rounded-lg" value="CONTINUE"></input>
+                    <div className="w-full">
+                        <input type="submit" className="text-white text-sm bg-black h-[50px] text-center w-full rounded-lg cursor-pointer" value="CONTINUE"></input>
                     </div>
                 </form>
             </div>
